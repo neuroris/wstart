436ce1e29e9385db1743e15bf972a87d9e123f3e
neuroris on jpc <neuroris@naver.com>
2024-05-24 12:14:41 +0900

    version 1.0

--- wstart.py
+++ wstart.py
@@ -126,35 +126,6 @@ class WookStart:
 
         return None
 
-    def start_hanaro_deprecated(self):
-        print('Hanaro start-up procedure begins')
-
-        app = Application('uia')
-        app.start('C:/OSSTEM/HANAROOK/hanaro.exe')
-        hanaro_dlg = app.window(title_re='^하나로 OK.*')
-        time.sleep(1)
-        hanaro_login_dlg = hanaro_dlg.window(title='로그인')
-
-        for count in range(10):
-            if hanaro_login_dlg.exists():
-                break
-            time.sleep(2)
-            hanaro_login_dlg = hanaro_dlg.window(title='로그인')
-
-        if not hanaro_login_dlg.exists():
-            print('Login failed : Maybe hanaro server is not turned on')
-            return
-
-        hanaro_password_dlg = hanaro_login_dlg.window(title='비밀 번호:', auto_id='1001', control_type="Edit")
-        hanaro_password_dlg.type_keys(self.hanaro_password)
-        pywinauto.keyboard.send_keys('{ENTER}')
-        time.sleep(0.5)
-
-        hanaro_today_dlg = hanaro_dlg.window(title_re='하나로 투데이')
-        if hanaro_today_dlg.exists():
-            hanaro_today_checkbox = hanaro_today_dlg.window(title='오늘은 열지 않음')
-            hanaro_today_checkbox.click_input()
-
     def start_hanaro(self):
         try:
             print('\nHanaro start-up procedure begins')
@@ -177,30 +148,6 @@ class WookStart:
         app = Application('uia')
         app.start('C:/PointNix/CDX-View/cdx_pointnix.exe')
         # Popen('C:/PointNix/CDX-View/cdx_pointnix.exe')
-
-    def start_ADT_deprecated(self):
-        print('ADT start-up procedure begins')
-        pyautogui.hotkey('ctrl', 'win', 'left')
-        pyautogui.hotkey('ctrl', 'win', 'left')
-
-        app = Application('uia')
-        app.start("C:/Program Files/ADT EYE3/EYE3.exe")
-
-        # wait ADT window ready
-        for count in range(1, 20):
-            try:
-                app.connect(title_re='ADT-EYE 3.0', control_type='Window')
-                break
-            except:
-                print('ADT window is not ready... trial({})'.format(count))
-                time.sleep(3)
-
-        adt_dlg = app.window(title_re='ADT-EYE 3.0')
-        adt_dlg.double_click_input(coords=(110, 250))
-        adt_dlg.click_input(coords=(150, 270), button_up=False)
-        adt_dlg.click_input(coords=(400, 275), button_down=False)
-
-        pyautogui.hotkey('ctrl', 'win', 'right')
 
     def start_ADT(self):
         try:
@@ -239,47 +186,6 @@ class WookStart:
         print('\nPyCharm start-up procedure begins')
         app = Application('uia')
         app.start('C:/Program Files/JetBrains/PyCharm Community Edition 2023.3.5/bin/pycharm64.exe')
-
-    def start_chrome_deprecated(self):
-        print('Chrome start-up procedure begins')
-        app = Application('uia')
-        app.start('C:/Program Files/Google/Chrome/Application/chrome.exe')
-        # app.connect(title_re='새 탭', control_type='Window')
-        chrome_dlg = app.top_window()
-        search_dlg = chrome_dlg.window(title_re='작업 영역')
-        search_dlg.click_input()
-
-        # chrome_dlg.print_control_identifiers()
-
-        # chrome_dlg = app.top_window()
-        # # chrome_dlg.maximize()
-        #
-        # chrome_search_dlg = chrome_dlg.window(title='주소창 및 검색창', control_type='Edit')
-        # chrome_search_dlg.type_keys('http://naver.com/')
-        # pywinauto.keyboard.send_keys('{ENTER}')
-        # chrome_mail_dlg = chrome_dlg.window(title='메일')
-        # chrome_mail_dlg.click_input()
-
-        # try:
-        #     chrome_newtag_dlg = chrome_dlg.window(title='새 탭', control_type='Button')
-        #     chrome_newtag_dlg.click_input()
-        #     chrome_search_dlg.type_keys('https://kebhana.com')
-        #     pywinauto.keyboard.send_keys('{ENTER}')
-        #     # # chrome_inquiry_dlg = chrome_dlg.window(title='조회')
-        #     # # chrome_inquiry_dlg.click_input()
-        #     # # chrome_total_inquiry_dlg = chrome_dlg.window(title='전체계좌조회')
-        #     # # chrome_total_inquiry_dlg.click_input()
-        #
-        #     chrome_newtag_dlg.click_input()
-        #     chrome_search_dlg.type_keys('https://remotedesktop.google.com')
-        #     pywinauto.keyboard.send_keys('{ENTER}')
-        #
-        #     chrome_newtag_dlg.click_input()
-        #     chrome_search_dlg.type_keys('https://remotedesktop.google.com')
-        #     pywinauto.keyboard.send_keys('{ENTER}')
-
-        # except Exception as e:
-        #     print('Something is wrong when open KEB bank', e)
 
     def start_chrome(self):
         try:
@@ -322,6 +228,7 @@ class WookStart:
             typing_message = '^a''{DELETE}' + self.message_password + '{ENTER}'
             password_dlg.type_keys(typing_message)
             self.wait_dlg(kakaotalk_dlg)
+            time.sleep(0.2)
             kakaotalk_dlg.minimize()
         except Exception as e:
             self.report_failure('KakaoTalk', e)
@@ -336,23 +243,10 @@ class WookStart:
             typing_message = '^a''{DELETE}' + self.message_password + '{ENTER}'
             password_dlg.type_keys(typing_message)
             self.wait_dlg(line_dlg)
+            time.sleep(0.2)
             line_dlg.minimize()
         except Exception as e:
             self.report_failure('LINE', e)
-
-    def start_millie_deprecated(self):
-        try:
-            print('millie start-up procedure begins')
-
-            app = Application('uia')
-            app.start('C:/Program Files/millie/millie.exe')
-            millie_dlg = app.top_window()
-            study_dlg = millie_dlg.window(title_re='내 서재')
-            time.sleep(4)
-            study_dlg.set_focus()
-            study_dlg.click_input()
-        except Exception as e:
-            self.report_failure('Millie', e)
 
     def start_millie(self):
         try:
@@ -367,10 +261,12 @@ class WookStart:
             self.report_failure('Millie', e)
 
     def start_kiwoom(self):
-        Popen('C:/KiwoomHero4/Bin/NKStarter.exe')
+        app = Application('uia')
+        app.start('C:/KiwoomHero4/Bin/NKStarter.exe')
 
     def start_efriend(self):
-        Popen('C:/eFriend Expert/efriendexpert/efriendexpert.exe')
+        app = Application('uia')
+        app.start('C:/eFriend Expert/efriendexpert/efriendexpert.exe')
 
 if __name__ == '__main__':
     ws = WookStart()
